235b2a896a7f3088a4a663b7ffd1093d3d5e1903
Ibrahim Zahoor <ibrahimzahoor.nu@gmail.com>
2017-03-21 15:07:48 +0500

    configure sass, babel

--- components/App.jsx
+++ components/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // console.log('constructor props', props);
+  }
+
   render() {
+    const a = 10;
     return (
     <div style={{ textAlign: 'center' }}>
-      <h1>Hello World</h1>
+      <h1>Hello World{a}</h1>
     </div>);
   }
 }
